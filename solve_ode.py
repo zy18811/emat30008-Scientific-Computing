c859fec074522f629b6d1e0e1e7078e71d475011
Luca Bennett <zy18811@bristol.ac.uk>
2021-02-25 16:58:49 +0000

    removed plotting stuff from solve_ode.py
    plotting stuff now going in its own file

--- solve_ode.py
+++ solve_ode.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-import math
-import numpy as np
-from scipy.integrate import odeint
 import sys
+
 
 def euler_step(f,x1,t1,h):
     x2 = x1+h*f(t1,x1)
@@ -28,8 +25,6 @@ def solve_to(step,f,x1,t1,t2,hmax):
     return x1
 
 
-
-
 def solve_ode(f,x0,t,hmax,method):
     if method == "rk4":
         step = rk4_step
@@ -43,45 +38,3 @@ def solve_ode(f,x0,t,hmax,method):
         x0 = solve_to(step,f,x0,t[i],t[i+1],hmax)
     x_sol.append(x0)
     return x_sol
-
-
-
-
-def func(t,x):
-    dxdt = x
-    return dxdt
-
-
-def getTrueValue(t):
-    x = np.exp(t)
-    return x
-
-
-def squaredError(true,est):
-    err = true-est
-    return err**2
-
-
-t = np.linspace(0,1,100)
-
-
-hArr = []
-errArrEul = []
-errArrRK4 = []
-for i in range(7):
-    h = 10**-i
-    print("h = %f" % h)
-    x1_est_eul = solve_ode(func,1,t,h,"euler")[-1]
-    x1_est_rk4 = solve_ode(func,1,t,h,"rk4")[-1]
-    x1_true = getTrueValue(1)
-    errEul = squaredError(x1_true,x1_est_eul)
-    errArrEul.append(errEul)
-    errEul = squaredError(x1_true, x1_est_rk4)
-    errArrRK4.append(errEul)
-    hArr.append(h)
-
-
-plt.plot(np.log(hArr),np.log(errArrEul))
-plt.plot(np.log(hArr),np.log(errArrRK4))
-plt.show()
-
