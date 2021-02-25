c803feced24ab65eb90e74095ab84d70bdf7e18b
Luca Bennett <zy18811@bristol.ac.uk>
2021-02-25 19:46:34 +0000

    loglog plot with h vs err for euler and rk4

--- errorplots.py
+++ errorplots.py
@@ -14,34 +14,36 @@ def getTrueValue(t):
     return x
 
 
-def squaredError(true,est):
-    err = true-est
-    return err**2
+def error(true,est):
+    err = est-true
+    return abs(err)
 
 
 def errorPlot(method, hvals, t,format,ax):
     errArr = []
-    tArr = [0, t]
+    tArr = [0,t]
+
     for h in hvals:
+
         x_est = solve_ode(func,1,tArr,h,method)[-1]
         x_true = getTrueValue(t)
-        err = squaredError(x_true,x_est)
+        err = error(x_true,x_est)
         errArr.append(err)
     plot(hvals,errArr,ax,format)
 
 
 def main():
-    h_vals = np.linspace(1,0.000000001,1000)
-    
+    #h_vals = np.linspace(5,0.0000001,100)
+    h_vals = np.linspace(0.1,0.000001,20)
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-   # errorPlot("euler",h_vals,1,"loglog",ax)
+    errorPlot("euler",h_vals,1,"loglog",ax)
     errorPlot("rk4",h_vals,1,"loglog",ax)
     plt.show()
 
 
 if __name__ == "__main__":
-   main()
+    main()
 
 
 
--- plot.py
+++ plot.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
-
+import sys
 
 def plot(x,y,ax,format):
     if format == "loglog":
         ax.loglog(x, y)
-    else:
+    elif format == "linear":
         ax.plot(x,y)
+    else:
+        sys.exit("format: \"%s\" invalid. Please input a valid format." % format)
+
 
