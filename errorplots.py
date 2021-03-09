4f05d7ad21168635f2b51fe084b0ce4d6a33517f
Luca Bennett <zy18811@bristol.ac.uk>
2021-03-09 09:50:36 +0000

    small changes

--- errorplots.py
+++ errorplots.py
@@ -21,24 +21,23 @@ def error(true,est):
 
 def errorPlot(method, hvals, t,format,ax):
     errArr = []
-    tArr = [0,t]
-
+    tArr = np.linspace(0,t,1000)
     for h in hvals:
-
         x_est = solve_ode(func,1,tArr,h,method)[-1]
         x_true = getTrueValue(t)
-        err = error(x_true,x_est)
+        err = abs(x_est-x_true)
         errArr.append(err)
     plot(hvals,errArr,ax,format)
 
 
 def main():
     #h_vals = np.linspace(5,0.0000001,100)
-    h_vals = np.linspace(0.1,0.000001,20)
+    h_vals = np.linspace(0.1,0.0001,20)
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     errorPlot("euler",h_vals,1,"loglog",ax)
     errorPlot("rk4",h_vals,1,"loglog",ax)
+    plt.legend(["Euler","RK4"])
     plt.show()
 
 
--- solve_ode.py
+++ solve_ode.py
@@ -1,35 +1,33 @@
 import sys
 import math
 
-def euler_step(f,x1,t1,h):
-    x2 = x1+h*f(t1,x1)
-    t2 = t1+h
-    return t2,x2
 
-
-def rk4_step(f,x1,t1,h):
-    m1 = f(t1,x1)
-    m2 = f(t1+h/2,x1+(h/2)*m1)
-    m3 = f(t1+h/2,x1+(h/2)*m2)
-    m4 = f(t1+h,x1+h*m3)
-    x2 = x1+(h/6)*(m1+2*m2+2*m3+m4)
+def euler_step(f, x1, t1, h):
+    x2 = x1 + h * f(t1, x1)
     t2 = t1 + h
-    return t2,x2
+    return t2, x2
 
 
-def solve_to(step,f,x1,t1,t2,hmax):
-    if hmax >(t2-t1):
-        hmax = t2-t1
+def rk4_step(f, x1, t1, h):
+    m1 = f(t1, x1)
+    m2 = f(t1 + h / 2, x1 + (h / 2) * m1)
+    m3 = f(t1 + h / 2, x1 + (h / 2) * m2)
+    m4 = f(t1 + h, x1 + h * m3)
+    x2 = x1 + (h / 6) * (m1 + 2 * m2 + 2 * m3 + m4)
+    t2 = t1 + h
+    return t2, x2
+
+
+def solve_to(step, f, x1, t1, t2, hmax):
     while t1<t2:
         t1,x1 = step(f,x1,t1,hmax)
     else:
         t1-=hmax
         t1,x1 = step(f,x1,t1,t2-t1)
-
     return x1
 
 
-def solve_ode(f,x0,t,hmax,method):
+def solve_ode(f, x0, t, hmax, method):
     if method == "rk4":
         step = rk4_step
     elif method == "euler":
@@ -37,10 +35,10 @@ def solve_ode(f,x0,t,hmax,method):
     else:
         sys.exit("Method: \"%s\" is not valid. Please select a valid method" % method)
     x_sol = []
-
-    for i in range(len(t)-1):
-
+    for i in range(len(t) - 1):
         x_sol.append(x0)
-        x0 = solve_to(step,f,x0,t[i],t[i+1],hmax)
+        x0 = solve_to(step, f, x0, t[i], t[i + 1], hmax)
     x_sol.append(x0)
     return x_sol
+
+
