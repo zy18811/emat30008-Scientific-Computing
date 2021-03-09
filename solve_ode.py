83ba5df1df5d88b4c440bafb4101992b87227d99
Luca Bennett <zy18811@bristol.ac.uk>
2021-03-09 16:23:19 +0000

    fixing bugs progress save

--- solve_ode.py
+++ solve_ode.py
@@ -5,7 +5,7 @@ import math
 def euler_step(f, x1, t1, h):
     x2 = x1 + h * f(t1, x1)
     t2 = t1 + h
-    return t2, x2
+    return x2, t2
 
 
 def rk4_step(f, x1, t1, h):
@@ -15,27 +15,31 @@ def rk4_step(f, x1, t1, h):
     m4 = f(t1 + h, x1 + h * m3)
     x2 = x1 + (h / 6) * (m1 + 2 * m2 + 2 * m3 + m4)
     t2 = t1 + h
-    return t2, x2
+    return x2, t2
 
 
 def solve_to(step,f,x1,t1,t2,hmax):
-    while True:
-        if (t1+hmax)>t2:
-            t1,x1 = step(f,x1,t1,t2-t1)
-            break
-        t1,x1 = step(f,x1,t1,hmax)
+    while (t1+hmax)<t2:
+        x1,t1 = step(f,x1,t1,hmax)
+    else:
+        x1,t1 = step(f,x1,t1,t2-t1)
     return x1
 
 
+
+
 def solve_ode(f,x0,tArr,method,hmax):
-    if method == "rk4":
-        step = rk4_step
-    elif method == "euler":
-        step = euler_step
+    if method == "euler":
+        method = euler_step
+    elif method == "rk4":
+        method = rk4_step
     else:
-        sys.exit("Method: \"%s\" is not valid. Please select a valid method" % method)
-    x_sol = [x0]
-    for i in range(len(tArr)-1):
-        xi = solve_to(step,f,x_sol[i],tArr[i],tArr[i+1],hmax)
-        x_sol.append(xi)
-    return x_sol
+
+
+def func(t,x):
+    dxdt = x
+    return dxdt
+
+
+x = solve_to(euler_step,func,1,0,1,0.01)
+print(x)
