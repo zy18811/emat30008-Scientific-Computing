dc5eb940b237a0ad8e40f93e8c41081c5875ccf1
Luca Bennett <zy18811@bristol.ac.uk>
2021-03-10 16:26:02 +0000

    Added bool 'system' to solve_ode, False does normal, True takes array

--- solve_ode.py
+++ solve_ode.py
@@ -28,45 +28,29 @@ def solve_to(step,f,x1,t1,t2,hmax):
     return x1
 
 
-def solve_ode(f,x0,tArr,method,hmax):
+def solve_ode(f,x0,tArr,method,hmax,system = False):
     if method == "euler":
         step = euler_step
     elif method == "rk4":
         step = rk4_step
     else:
         sys.exit("Method: \"%s\" is not valid. Please select a valid method" % method)
-    sol = np.empty(shape = (len(tArr),len(x0)))
+    if system:
+        sol = np.empty(shape = (len(tArr),len(x0)))
+    else:
+        sol = np.empty(shape=(len(tArr), 1))
     sol[0] = x0
     for i in range(len(tArr)-1):
         xi = solve_to(step,f,sol[i],tArr[i],tArr[i+1],hmax)
         sol[i+1] = xi
-    outputSol = np.empty(shape = (len(x0),len(tArr)))
-    for i in range(len(x0)):
-        outputSol[i] = [item[i] for item in sol]
-    return outputSol
+    if system:
+        outputSol = np.empty(shape = (len(x0),len(tArr)))
+        for i in range(len(x0)):
+            outputSol[i] = [item[i] for item in sol]
+        return outputSol
+    else:
+        return sol
 
 
-def f(t,y):
-    return np.array([y[1],-y[0]])
-
-def func(t,x):
-    dxdt = x
-    return dxdt
 
 
-t = np.linspace(0,10,100)
-sol = solve_ode(func,[1],t,"rk4",0.01)
-'''
-x = []
-y = []
-for item in sol:
-    x.append(item[0])
-    y.append(item[1])
-'''
-x = sol[0]
-#y = sol[1]
-x_true = np.sin(t)
-y_true = np.cos(t)
-plt.plot(t,x)
-#plt.plot(t,y_true)
-plt.show()
